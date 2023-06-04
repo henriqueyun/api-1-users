5a8a48ed8c048db470cc2e25c93cf6695de8f8a1
henriqueyun <henriqueyun@gmail.com>
2023-06-03 21:28:13 -0300

    tests: setup to use MMS

--- tests/setup.js
+++ tests/setup.js
@@ -1,2 +1,12 @@
+const { MongoMemoryServer } = require("mongodb-memory-server");
 const { getEnv } = require("../config/dotenv");
-getEnv({ test: true });
+
+async function setupTestEnvironment() {
+    getEnv({ test: true });
+    const mongoServer = await MongoMemoryServer.create();
+    console.log("before", process.env.DB_URL);
+    process.env.DB_URL = mongoServer.getUri();
+    console.log("after", process.env.DB_URL);
+}
+
+setupTestEnvironment();
