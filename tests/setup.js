74f0b2be605d15332a67c36c211c94049d1dd140
henriqueyun <henriqueyun@gmail.com>
2023-06-03 21:43:01 -0300

    fix: turning async setup into a iife

--- tests/setup.js
+++ tests/setup.js
@@ -1,12 +1,8 @@
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const { getEnv } = require("../config/dotenv");
 
-async function setupTestEnvironment() {
+(async function () {
     getEnv({ test: true });
     const mongoServer = await MongoMemoryServer.create();
-    console.log("before", process.env.DB_URL);
     process.env.DB_URL = mongoServer.getUri();
-    console.log("after", process.env.DB_URL);
-}
-
-setupTestEnvironment();
+})();
